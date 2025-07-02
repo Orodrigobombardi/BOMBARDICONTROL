93aa421dd9ffdb07299904a982d7e9445f03a5d8
Rodrigo Bombardi <rbombardi@icloud.com>
2025-07-02 10:42:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+materiais = {
+  "Metais": [ { nome, quantidade, categoria } ],
+  "Parafusos": [ { nome, quantidade, categoria } ],
+}
+function apagarCategoria(categoria) {
+  if (confirm(`Tem certeza que deseja apagar a categoria "${categoria}" com todos os materiais?`)) {
+    delete materiais[categoria];
+    salvarLocal();
+    atualizarCategorias();
+    atualizarLista();
+  }
+}
